76d584efb9099e72d6ed77355a71f1ce9d84128a
Iulian Caluian <iulian.coder@gmail.com>
2023-03-10 22:43:13 +0200

    Adding graph class

---
+++ Algorithms.Fundamentals/Chapter41/Graph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fundamentals.Chapter41
+{
+    public class Graph
+    {
+        public  int V { get; }
+        public int E { get; private set; }
+
+        private LinkedList<int>[] _adj;
+
+        public Graph(int v) {
+            V = v;
+            E = 0;
+            _adj = new LinkedList<int>[V];
+            for (int i = 0; i < V; i++) {
+                _adj[i] = new LinkedList<int>();    
+            }
+        }
+
+        // public Graph(Stream inputStream) { }
+
+        public void addEdge(int v, int w)
+        {
+            _adj[v].AddLast(w);
+            _adj[w].AddLast(v);
+            E++;
+        }
+        
+
+        public IEnumerable<int> adj(int v)
+        {
+            return _adj[v];
+        }
+
+        public override string ToString()
+        {
+            String s = V + " vertices, " + E + " edges\n";
+            for (int v = 0; v < V; v++)
+            {
+                s += v + ": ";
+                foreach (int w in this.adj(v))
+                    s += w + " ";
+                s += "\n";
+            }
+            return s;
+        }
+
+
+
+    }
+}
---
+++ Algorithms.Fundamentals/Chapter41/UnidrectedGraphLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fundamentals.Chapter41
+{
+    public static class UnidrectedGraphLibrary
+    {
+        public static int degree(Graph g, int v)
+        {
+            return g.adj(v).Count();
+        }
+
+        public static int maxDegree(Graph g)
+        {
+            int max = 0;
+            for (int v = 0; v < g.V; v++)
+                if (degree(g, v) > max)
+                    max = degree(g, v);
+            return max;
+        }
+        public static double averageDegree(Graph g)
+        {
+            return 2.0 * g.E / g.V;
+        }
+    }
+}

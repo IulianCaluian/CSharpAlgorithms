798a06c2b6896ba52e1477e61fe471fc80e00553
Iulian Caluian <iulian.coder@gmail.com>
2023-03-20 22:57:37 +0200

    Add cycle detection

---
+++ Algorithms.Fundamentals/Chapter41/Cycle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fundamentals.Chapter41
+{
+    internal class Cycle
+    {
+        private bool[] marked;
+        private bool hasCycle;
+        public Cycle(Graph g)
+        {
+            marked = new bool[g.V];
+            for (int v = 0; v < g.V; v++)
+                if (!marked[v])
+                    dfs(g, v, v);
+        }
+
+        private void dfs(Graph G, int v, int u)
+        {
+            marked[v] = true;
+            foreach(int w in G.adj(v))
+                if (!marked[w])
+                    dfs(G, w, v);
+                else if (w != u) 
+                    hasCycle = true;
+        }
+
+        public bool HasCycle()
+        {
+            return hasCycle;
+        }
+    }
+}

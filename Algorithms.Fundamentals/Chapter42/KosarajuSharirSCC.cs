a3a037a5f0b09748aa9c97da92da3ecaf34ace6f
Iulian Caluian <iulian.coder@gmail.com>
2023-04-05 22:55:31 +0300

    Strongly connected components

---
+++ Algorithms.Fundamentals/Chapter42/KosarajuSharirSCC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fundamentals.Chapter42
+{
+    internal class KosarajuSharirSCC
+    {
+        private bool[] marked;
+        private int[] id;
+        private int count;
+        public KosarajuSharirSCC(Digraph g)
+        {
+            marked = new bool[g.V];
+            id = new int[g.V];
+
+            DepthFirstOrder order = new DepthFirstOrder(g.reverse());
+
+            foreach (int s in order.ReversePost())
+            {
+                if (!marked[s])
+                    dfs(g, s);
+            }
+        }
+        private void dfs(Digraph G, int v)
+        {
+            marked[v] = true;
+            id[v] = count;
+            foreach (int w in G.adj(v))
+                if (!marked[w])
+                    dfs(G, w);
+        }
+
+        public bool stlronglyConnected(int v, int w)
+        { return id[v] == id[w]; }
+        public int Id(int v)
+        { return id[v]; }
+        public int Count()
+        { return count; }
+
+    }
+
+}
